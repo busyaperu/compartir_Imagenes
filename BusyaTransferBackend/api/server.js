6e334181c88eff2cf646fc99f3d9fc97d03c008f
jorge <busyaperu@gmail.com>
2024-11-28 18:02:33 -0500

    Moved server.js to api folder

--- BusyaTransferBackend/api/server.js
+++ BusyaTransferBackend/api/server.js
@@ -75,3 +75,6 @@ app.post("/process-image", async (req, res) => {
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
+// Exportar la aplicación para que Vercel la reconozca
+module.exports = app;
+
